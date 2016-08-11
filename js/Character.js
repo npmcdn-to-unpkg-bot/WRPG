7fb4c1bf3b187969d3af2db8328882e772d71c35
Michael Wood <mwooddesigns@gmail.com>
2016-08-10 22:47:34 -0400

    Initial Character prototype setup with getters and setters.

---
+++ js/Character.js
@@ -0,0 +1,146 @@
+function Character(id, name, race, level, maxHealth, maxEnergy, maxMana, strength, agility, intelligence, charisma, xpos, ypos) {
+  this.character_id = id;
+  this.character_name = name;
+  this.character_race = race;
+  this.character_level = level;
+  this.character_health = maxHealth;
+  this.character_max_health = maxHealth;
+  this.character_energy = maxEnergy;
+  this.character_max_engery = maxEnergy;
+  this.character_mana = maxMana;
+  this.chracter_max_mana = maxMana;
+  this.character_strength = strength;
+  this.character_agility = agility;
+  this.character_intelligence = intelligence;
+  this.character_charisma = charisma;
+  this.xpos = xpos;
+  this.ypos = ypos;
+}
+
+Character.prototype.getID = function() {
+  return this.character_id;
+}
+
+Character.prototype.getName = function() {
+  return this.character_name;
+}
+
+Character.prototype.getRace = function() {
+  return this.character_race;
+}
+
+Character.prototype.getLevel = function() {
+  return this.character_level;
+}
+
+Character.prototype.getHealth = function() {
+  return this.character_health;
+}
+
+Character.prototype.getMaxHealth = function() {
+  return this.character_max_health;
+}
+
+Character.prototype.getEnergy = function() {
+  return this.character_energy;
+}
+
+Character.prototype.getMaxEnergy = function() {
+  return this.character_max_energy;
+}
+
+Character.prototype.getMana = function() {
+  return this.character_mana;
+}
+
+Character.prototype.getMaxMana = function() {
+  return this.character_max_mana;
+}
+
+Character.prototype.getStrength = function() {
+  return this.character_strength;
+}
+
+Character.prototype.getAgility = function() {
+  return this.character_agility;
+}
+
+Character.prototype.getIntelligence = function() {
+  return this.character_intelligence;
+}
+
+Character.prototype.getCharisma = function() {
+  return this.character_charisma;
+}
+
+Character.prototype.getXPos = function() {
+  return this.xpos;
+}
+
+Character.prototype.getYPos = function() {
+  return this.ypos;
+}
+
+Character.prototype.setID = function(val) {
+  this.character_id = val;
+}
+
+Character.prototype.setName = function(str) {
+  this.character_name = str;
+}
+
+Character.prototype.setRace = function(str) {
+  this.character_race = str;
+}
+
+Character.prototype.setLevel = function(val) {
+  this.character_level = val;
+}
+
+Character.prototype.setHealth = function(val) {
+  this.character_health = val;
+}
+
+Character.prototype.setMaxHealth = function(val) {
+  this.character_max_health = val;
+}
+
+Character.prototype.setEnergy = function(val) {
+  this.character_energy = val;
+}
+
+Character.prototype.setMaxEnergy = function(val) {
+  this.character_max_energy = val;
+}
+
+Character.prototype.setMana = function(val) {
+  this.character_mana = val;
+}
+
+Character.prototype.setMaxMana = function(val) {
+  this.character_max_mana = val;
+}
+
+Character.prototype.setStrength = function(val) {
+  this.character_strength = val;
+}
+
+Character.prototype.setAgility = function(val) {
+  this.character_agility = val;
+}
+
+Character.prototype.setIntelligence = function(val) {
+  this.character_intelligence = val;
+}
+
+Character.prototype.setCharisma = function(val) {
+  this.character_charisma = val;
+}
+
+Character.prototype.setXPos = function(val) {
+  this.xpos = val;
+}
+
+Character.prototype.setYPos = function(val) {
+  this.ypos = val;
+}
---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Eventually the main controller. For now a bunch of console logging.
+
+//Character(id, name, race, level, maxHealth, maxEnergy, maxMana, strength, agility, intelligence, charisma, xpos, ypos)
+
+var test = new Character(1, "Rupert", "Human", 1, 100, 50, 25, 9, 7, 4, 5, 0, 0);
+
+console.log(test.getName());
+console.log(test.getHealth());
+
+console.log(test.getStrength());
